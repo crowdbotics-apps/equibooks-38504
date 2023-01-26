985b6e206bb4212a858dae236caa1f10f39b0f7c
Crowdbotics <team@crowdbotics.com>
2023-01-26 21:19:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "equibooks-38504",
+  url: "https://equibooks-38504.botics.co",
+  api: "https://equibooks-38504.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
